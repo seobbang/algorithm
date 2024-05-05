de67f095dd9f1b1f6cc9c7e4af8c03abe51d199e
seobbang <seobbangway@naver.com>
2024-05-05 17:44:36 +0900

    0505 - hash #1 (refactor)

--- hash/폰켓몬.js
+++ hash/폰켓몬.js
@@ -1,10 +1,5 @@
 function solution(nums) {
   const N = nums.length;
-
-  const array = [];
-  nums.forEach((num) => {
-    if (!array.includes(num)) array.push(num);
-  });
-
+  const array = [...new Set(nums)];
   return array.length >= N / 2 ? N / 2 : array.length;
 }
